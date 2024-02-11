179313dd602dccb477e7d5c9405cb92a56b1e24f
Ierga <i.ergakoff@gmail.com>
2024-02-11 22:08:38 +0300

    Initial commit

---
+++ src/main/java/ru/ergakov/gb/GbSpringHomework5Application.java
@@ -0,0 +1,13 @@
+package ru.ergakov.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbSpringHomework5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbSpringHomework5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ergakov/gb/GbSpringHomework5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ergakov.gb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbSpringHomework5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
